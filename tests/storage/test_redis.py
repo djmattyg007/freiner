1fd9f6af17ac6f94efe1ea2a712499560e210d75
Matthew Gamble <git@matthewgamble.net>
2021-08-03 21:48:41 +1000

    Whitespace improvements

--- tests/storage/test_redis.py
+++ tests/storage/test_redis.py
@@ -17,48 +17,60 @@ class SharedRedisTests(object):
     def test_fixed_window(self):
         limiter = FixedWindowRateLimiter(self.storage)
         per_second = RateLimitItemPerSecond(10)
+
         start = time.time()
         count = 0
         while time.time() - start < 0.5 and count < 10:
             self.assertTrue(limiter.hit(per_second))
             count += 1
         self.assertFalse(limiter.hit(per_second))
+
         while time.time() - start <= 1:
             time.sleep(0.1)
         [self.assertTrue(limiter.hit(per_second)) for _ in range(10)]
 
     def test_reset(self):
         limiter = FixedWindowRateLimiter(self.storage)
+
         for i in range(0, 10):
             rate = RateLimitItemPerMinute(i)
             limiter.hit(rate)
+
         self.assertEqual(self.storage.reset(), 10)
 
     def test_fixed_window_clear(self):
         limiter = FixedWindowRateLimiter(self.storage)
         per_min = RateLimitItemPerMinute(1)
+
         limiter.hit(per_min)
         self.assertFalse(limiter.hit(per_min))
+
         limiter.clear(per_min)
         self.assertTrue(limiter.hit(per_min))
 
     def test_moving_window_clear(self):
         limiter = MovingWindowRateLimiter(self.storage)
         per_min = RateLimitItemPerMinute(1)
+
         limiter.hit(per_min)
         self.assertFalse(limiter.hit(per_min))
+
         limiter.clear(per_min)
         self.assertTrue(limiter.hit(per_min))
 
     def test_moving_window_expiry(self):
         limiter = MovingWindowRateLimiter(self.storage)
         limit = RateLimitItemPerSecond(2)
+
         self.assertTrue(limiter.hit(limit))
+
         time.sleep(0.9)
         self.assertTrue(limiter.hit(limit))
         self.assertFalse(limiter.hit(limit))
+
         time.sleep(0.1)
         self.assertTrue(limiter.hit(limit))
+
         last = time.time()
         while time.time() - last <= 1:
             time.sleep(0.05)
