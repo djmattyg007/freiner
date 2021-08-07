2f8f7530ea4dea9b4250f7ca556a95f0a753ac60
Matthew Gamble <git@matthewgamble.net>
2021-08-07 15:26:36 +1000

    Add more tests for limit item representation

--- tests/test_limit_granularities.py
+++ tests/test_limit_granularities.py
@@ -10,13 +10,22 @@ def test_seconds_value():
     assert limits.RateLimitItemPerYear(1).get_expiry() == 12 * 30 * 24 * 60 * 60
 
 
-def test_representation():
+def test_str_representation():
     assert str(limits.RateLimitItemPerSecond(1)) == "1 per 1 second"
     assert str(limits.RateLimitItemPerMinute(1)) == "1 per 1 minute"
     assert str(limits.RateLimitItemPerHour(1)) == "1 per 1 hour"
     assert str(limits.RateLimitItemPerDay(1)) == "1 per 1 day"
     assert str(limits.RateLimitItemPerMonth(1)) == "1 per 1 month"
     assert str(limits.RateLimitItemPerYear(1)) == "1 per 1 year"
+
+
+def test_repr_representation():
+    assert repr(limits.RateLimitItemPerSecond(2)) == "RateLimitItemPerSecond<2 per 1 second>"
+    assert repr(limits.RateLimitItemPerMinute(3)) == "RateLimitItemPerMinute<3 per 1 minute>"
+    assert repr(limits.RateLimitItemPerHour(4)) == "RateLimitItemPerHour<4 per 1 hour>"
+    assert repr(limits.RateLimitItemPerDay(5)) == "RateLimitItemPerDay<5 per 1 day>"
+    assert repr(limits.RateLimitItemPerMonth(6)) == "RateLimitItemPerMonth<6 per 1 month>"
+    assert repr(limits.RateLimitItemPerYear(7)) == "RateLimitItemPerYear<7 per 1 year>"
 
 
 def test_comparison():
--- tests/test_limits.py
+++ tests/test_limits.py
@@ -23,3 +23,8 @@ def test_key_with_int_default_namespace():
 def test_key_with_mixed_string_types_default_namespace():
     item = FakeLimit(1, 1)
     assert item.key_for(b"a", "b") == "LIMITER/a/b/1/1/fake"
+
+
+def test_key_with_object_default_namespace():
+    item = FakeLimit(1, 1)
+    assert item.key_for({"a": 1, "b": 2}) == "LIMITER/{'a': 1, 'b': 2}/1/1/fake"
